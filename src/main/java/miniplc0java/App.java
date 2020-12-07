6b6e2f394fe49ddb1aa624554cb3813e1390f105
xiangjie3291 <985812032@qq.com>
2020-12-07 16:51:43 +0800

    hello

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -1,7 +1,6 @@
 package miniplc0java;
 
 import miniplc0java.Struct.FunctionDef;
-import miniplc0java.Struct.GlobalDef;
 import miniplc0java.analyser.Analyser;
 import miniplc0java.analyser.BC;
 import miniplc0java.tokenizer.StringIter;
@@ -15,11 +14,11 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         try {
-            String a = "1.0e10";
-            double b = Double.parseDouble(a);
-            long l = Double.doubleToLongBits(b);
-            System.out.println(b);
-            System.out.println(Long.toBinaryString(l));
+//            String a = "1.0e10";
+//            double b = Double.parseDouble(a);
+//            long l = Double.doubleToLongBits(b);
+//            System.out.println(b);
+//            System.out.println(Long.toBinaryString(l));
 
 
             InputStream inputStream = new FileInputStream(args[0]);
@@ -29,10 +28,10 @@ public class App {
             var iter = new StringIter(scanner);
             Analyser tmp = new Analyser(new Tokenizer(iter));
             tmp.analyseProgram();
-
-            for (GlobalDef globalDef : tmp.getGlobalTable()) {
-                System.out.println(globalDef);
-            }
+//
+//            for (GlobalDef globalDef : tmp.getGlobalTable()) {
+//                System.out.println(globalDef);
+//            }
 
             List<Map.Entry<String, FunctionDef>> FunctionList = new ArrayList<Map.Entry<String, FunctionDef>>(tmp.getFunctionTable().entrySet());
 
@@ -43,14 +42,14 @@ public class App {
                     return (o1.getValue().getFunctionId() - o2.getValue().getFunctionId());
                 }
             });
-
-            for (Map.Entry<String, FunctionDef> functionDef : FunctionList) {
-                System.out.println(functionDef.getValue().getName());
-                System.out.println(functionDef);
-            }
+//
+//            for (Map.Entry<String, FunctionDef> functionDef : FunctionList) {
+//                System.out.println(functionDef.getValue().getName());
+//                System.out.println(functionDef);
+//            }
 
             BC output = new BC(tmp.getGlobalTable(), FunctionList);
-            System.out.println();
+//            System.out.println();
             DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(args[1])));
             List<Byte> bytes = output.getBcOut();
             byte[] resultBytes = new byte[bytes.size()];
